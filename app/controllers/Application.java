afe131bc4676d9cdd563374a1a29d7443d26b012
alex <mail@github.de>
2015-04-19 09:19:44 +0200

    Hello World WebSocket hinzugefügt;

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,9 +1,9 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import views.html.*;
+import play.mvc.Controller;
+import play.mvc.Result;
+import play.mvc.WebSocket;
+import views.html.index;
 
 public class Application extends Controller {
 
@@ -11,4 +11,32 @@ public class Application extends Controller {
         return ok(index.render("Your new application is ready."));
     }
 
+    public static WebSocket<String> wstest() {
+        return new WebSocket<String>() {
+
+            // Called when the Websocket Handshake is done.
+            public void onReady(WebSocket.In<String> in, WebSocket.Out<String> out) {
+
+                // For each event received on the socket,
+                in.onMessage(event -> {
+
+                    // Log events to the console
+                    System.out.println(event);
+
+                });
+
+                // When the socket is closed.
+                in.onClose(() -> System.out.println("Disconnected"));
+
+                // Send a single 'Hello!' message
+                out.write("Hello!");
+
+            }
+
+        };
+    }
+
+    public static WebSocket<String> wsactor() {
+        return WebSocket.withActor(MyWebSocketActor::props);
+    }
 }
---
+++ app/controllers/MyWebSocketActor.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.actor.UntypedActor;
+
+public class MyWebSocketActor extends UntypedActor {
+
+    public static Props props(ActorRef out) {
+        return Props.create(MyWebSocketActor.class, out);
+    }
+
+    private final ActorRef out;
+
+    public MyWebSocketActor(ActorRef out) {
+        this.out = out;
+    }
+
+    public void onReceive(Object message) throws Exception {
+        if (message instanceof String) {
+            out.tell("I received your message: " + message, self());
+        }
+    }
+}
