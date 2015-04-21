f1d2f34513935fd9fb0ea182774ca654796abb17
alex <ABergBN@users.noreply.github.com>
2015-04-21 14:50:42 +0200

    WebSocket HelloWorld hinzugefügt;

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,29 +8,22 @@ import views.html.index;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render("Hallo"));
     }
 
     public static WebSocket<String> wstest() {
         return new WebSocket<String>() {
 
-            // Called when the Websocket Handshake is done.
+            // Wird beim initialen Verboindungsaufbau aufgerufen
             public void onReady(WebSocket.In<String> in, WebSocket.Out<String> out) {
 
                 // For each event received on the socket,
                 in.onMessage(event -> {
-
-                    // Log events to the console
-                    System.out.println(event);
-
+                    out.write(event);
                 });
 
                 // When the socket is closed.
                 in.onClose(() -> System.out.println("Disconnected"));
-
-                // Send a single 'Hello!' message
-                out.write("Hello!");
-
             }
 
         };
