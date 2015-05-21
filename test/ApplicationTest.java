ed2e03eb8b63d802a2ad027ad7c758dadb2ce703
alex <ABergBN@users.noreply.github.com>
2015-05-21 15:33:00 +0200

    Nachricht

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -1,3 +1,14 @@
+import model.BenutzerDao;
+import org.junit.Test;
+
 public class ApplicationTest {
+
+    @Test
+    public void BenutzerTest(){
+        BenutzerDao dao = new BenutzerDao();
+        dao.registerUser("name", "email@web.de", "geheim");
+    }
+
+
 
 }
