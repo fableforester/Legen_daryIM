7d5267dd800fd27941a85d234554b7f3ca56ea8d
Moritz <mohillspam@gmx.com>
2015-05-24 19:37:14 +0200

    text wird jetzt in scrollbarem div angezeigt

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -7,16 +7,40 @@ function init() {
 }
 
 function writeMessageToScreen(message) {
-    var pre = document.createElement("p");
-    pre.innerHTML = message;
-    output.appendChild(pre);
+
+    //var pre = document.createElement("p");
+    //pre.innerHTML = message;
+    //output.appendChild(pre);
+     var newcontent = document.createElement("p");
+    newcontent.id = 'message_id';
+    newcontent.appendChild(document.createTextNode(message));
+
+     /*
+    var theDiv = document.getElementById("chat");
+    var content = document.createTextNode(message+"<br>");
+    theDiv.appendChild(content);*/
+
+
+    /* var scr = document.getElementById('chat');
+    scr.parentNode.insertBefore(newcontent, scr);
+
+     */
+     var div = document.getElementById('chat');
+     div.innerHTML = div.innerHTML + message+'<br>';
+     div.scrollTop = div.scrollHeight;
+
+
+
+
+
 }
 
 function initChangedHandler() {
     var kontaktListeElement = document.getElementById('kontaktListe');
     kontaktListeElement.onchange = function() {
-        output.innerHTML = "";
+       // output.innerHTML = "";
         writeMessageToScreen('Kontakt zu ' + kontaktListeElement.options[kontaktListeElement.selectedIndex].text + ' geändert');
+
     }
 }
 
